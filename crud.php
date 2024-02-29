0b8b4e41cca7e1ddfef5a78f60f0ed193d672363
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-03-01 00:49:09 +0800

    Create add crud.

--- crud.php
+++ crud.php
@@ -1,5 +1,5 @@
 <?php
-include 'database/dbconfig.php';
+include 'database.php';
 
 function executePreparedStatement($conn, $query, $params, $types = "")
 {
@@ -19,46 +19,50 @@ function executePreparedStatement($conn, $query, $params, $types = "")
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // academic year add
-    if (isset($_POST['year-add'])) {
-        $academicYear = mysqli_real_escape_string($conn, $_POST['academicYear']);
-        $query        = "INSERT INTO academic_year (year) VALUES (?)";
-        $params       = [$academicYear];
-        if (executePreparedStatement($conn, $query, $params, "s")) {
-            header("Location: academic_year.php");
+    if (isset($_POST['stock-add'])) {
+        $item        = mysqli_real_escape_string($conn, $_POST['item']);
+        $description = mysqli_real_escape_string($conn, $_POST['description']);
+        $uom         = mysqli_real_escape_string($conn, $_POST['uom']);
+        $stockno     = mysqli_real_escape_string($conn, $_POST['stockno']);
+        $reorder     = mysqli_real_escape_string($conn, $_POST['reorder']);
+        $query       = "INSERT INTO item (item,description,unit_measure,stock_no,re_order) VALUES (?,?,?,?,?)";
+        $params      = [$item, $description, $uom, $stockno, $reorder];
+        if (executePreparedStatement($conn, $query, $params, "sssss")) {
+            header("Location: index.php");
         }
     }
     // academic year update status
-    if (isset($_POST['update-status'])) {
-        $academicYearStatus = mysqli_real_escape_string($conn, $_POST['academicStatus']);
-        $primaryId          = mysqli_real_escape_string($conn, $_POST['primaryId']);
-        $query              = "UPDATE academic_year SET status = ? WHERE id = ?";
-        $params             = [$academicYearStatus, $primaryId];
-        if (executePreparedStatement($conn, $query, $params, "ii")) {
-            header("Location: academic_year.php");
-        }
-    }
-    // courses.php add
-    if (isset($_POST['add-courses'])) {
-        $course  = mysqli_real_escape_string($conn, $_POST['course']);
-        $level   = mysqli_real_escape_string($conn, $_POST['level']);
-        $section = mysqli_real_escape_string($conn, $_POST['section']);
-        $query   = "INSERT INTO courses (course, course_code, level, section) VALUES (?, ?, ?, ?)";
-        $params  = [$course, $course, $level, $section];
-        if (executePreparedStatement($conn, $query, $params, "ssss")) {
-            header("Location: courses.php");
-        }
-    }
-    // course.php edit
-    if (isset($_POST['submit-edit-course'])) {
-        $id           = mysqli_real_escape_string($conn, $_POST['course_id']);
-        $edit_course  = mysqli_real_escape_string($conn, $_POST['edit-course']);
-        $edit_level   = mysqli_real_escape_string($conn, $_POST['edit-level']);
-        $edit_section = mysqli_real_escape_string($conn, $_POST['edit-section']);
-        $query        = "UPDATE courses SET course = ?, course_code = ?, level = ?, section = ? WHERE id = ?";
-        $params       = [$edit_course, $edit_course, $edit_level, $edit_section, $id];
-        if (executePreparedStatement($conn, $query, $params, "ssssi")) {
-            header("Location: courses.php");
-        }
-    }
+    // if (isset($_POST['update-status'])) {
+    //     $academicYearStatus = mysqli_real_escape_string($conn, $_POST['academicStatus']);
+    //     $primaryId          = mysqli_real_escape_string($conn, $_POST['primaryId']);
+    //     $query              = "UPDATE academic_year SET status = ? WHERE id = ?";
+    //     $params             = [$academicYearStatus, $primaryId];
+    //     if (executePreparedStatement($conn, $query, $params, "ii")) {
+    //         header("Location: academic_year.php");
+    //     }
+    // }
+    // // courses.php add
+    // if (isset($_POST['add-courses'])) {
+    //     $course  = mysqli_real_escape_string($conn, $_POST['course']);
+    //     $level   = mysqli_real_escape_string($conn, $_POST['level']);
+    //     $section = mysqli_real_escape_string($conn, $_POST['section']);
+    //     $query   = "INSERT INTO courses (course, course_code, level, section) VALUES (?, ?, ?, ?)";
+    //     $params  = [$course, $course, $level, $section];
+    //     if (executePreparedStatement($conn, $query, $params, "ssss")) {
+    //         header("Location: courses.php");
+    //     }
+    // }
+    // // course.php edit
+    // if (isset($_POST['submit-edit-course'])) {
+    //     $id           = mysqli_real_escape_string($conn, $_POST['course_id']);
+    //     $edit_course  = mysqli_real_escape_string($conn, $_POST['edit-course']);
+    //     $edit_level   = mysqli_real_escape_string($conn, $_POST['edit-level']);
+    //     $edit_section = mysqli_real_escape_string($conn, $_POST['edit-section']);
+    //     $query        = "UPDATE courses SET course = ?, course_code = ?, level = ?, section = ? WHERE id = ?";
+    //     $params       = [$edit_course, $edit_course, $edit_level, $edit_section, $id];
+    //     if (executePreparedStatement($conn, $query, $params, "ssssi")) {
+    //         header("Location: courses.php");
+    //     }
+    // }
 }
 ?>
--- index.php
+++ index.php
@@ -32,25 +32,24 @@
                 <h1 class="modal-title fs-5" id="exampleModalLabel">Add Items</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form id="addItemData">
+            <form action="crud.php" method="post">
                 <div class="modal-body">
-
                     <div class="form-group mb-2">
                         <label for="" class="form-label">Item:</label>
-                        <input type="text" class="form-control mb-1">
+                        <input type="text" class="form-control mb-1" name="item">
                         <label for="" class="form-label">Description:</label>
-                        <input type="text" class="form-control mb-1">
+                        <input type="text" class="form-control mb-1" name="description">
                         <label for="" class="form-label">Unit of Measurement:</label>
-                        <input type="text" class="form-control mb-1">
+                        <input type="text" class="form-control mb-1" name="uom">
                         <label for="" class="form-label">Stock No:</label>
-                        <input type="text" class="form-control mb-1">
+                        <input type="text" class="form-control mb-1" name="stockno">
                         <label for="" class="form-label">Re-order Point:</label>
-                        <input type="text" class="form-control mb-1">
+                        <input type="text" class="form-control mb-1" name="reorder">
                     </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                    <button type="submit" class="btn btn-primary">Add</button>
+                    <button type="submit" name="stock-add" class="btn btn-primary">Add</button>
                 </div>
             </form>
         </div>
