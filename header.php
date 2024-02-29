0502a04af34d4bed34dba4efc030c87893a267d1
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-03-01 00:27:50 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stock Card System</title>
+    <link rel="stylesheet" href="main_ui/css/bootstrap.min.css">
+    <script type="text/javascript" src="main_ui/js/bootstrap.js"></script>
+</head>
