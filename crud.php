91a2b73e00830697b7702de0424a1ae12accd0c2
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-03-01 00:24:29 +0800

    Initial crud.

---
+++ crud.php
@@ -0,0 +1,64 @@
+<?php
+include 'database/dbconfig.php';
+
+function executePreparedStatement($conn, $query, $params, $types = "")
+{
+    $stmt = $conn->prepare($query);
+    if ($stmt) {
+        if ($types != "") {
+            $stmt->bind_param($types, ...$params);
+        }
+        if ($stmt->execute()) {
+            $stmt->close();
+            return true;
+        }
+        $stmt->close();
+    }
+    return false;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // academic year add
+    if (isset($_POST['year-add'])) {
+        $academicYear = mysqli_real_escape_string($conn, $_POST['academicYear']);
+        $query        = "INSERT INTO academic_year (year) VALUES (?)";
+        $params       = [$academicYear];
+        if (executePreparedStatement($conn, $query, $params, "s")) {
+            header("Location: academic_year.php");
+        }
+    }
+    // academic year update status
+    if (isset($_POST['update-status'])) {
+        $academicYearStatus = mysqli_real_escape_string($conn, $_POST['academicStatus']);
+        $primaryId          = mysqli_real_escape_string($conn, $_POST['primaryId']);
+        $query              = "UPDATE academic_year SET status = ? WHERE id = ?";
+        $params             = [$academicYearStatus, $primaryId];
+        if (executePreparedStatement($conn, $query, $params, "ii")) {
+            header("Location: academic_year.php");
+        }
+    }
+    // courses.php add
+    if (isset($_POST['add-courses'])) {
+        $course  = mysqli_real_escape_string($conn, $_POST['course']);
+        $level   = mysqli_real_escape_string($conn, $_POST['level']);
+        $section = mysqli_real_escape_string($conn, $_POST['section']);
+        $query   = "INSERT INTO courses (course, course_code, level, section) VALUES (?, ?, ?, ?)";
+        $params  = [$course, $course, $level, $section];
+        if (executePreparedStatement($conn, $query, $params, "ssss")) {
+            header("Location: courses.php");
+        }
+    }
+    // course.php edit
+    if (isset($_POST['submit-edit-course'])) {
+        $id           = mysqli_real_escape_string($conn, $_POST['course_id']);
+        $edit_course  = mysqli_real_escape_string($conn, $_POST['edit-course']);
+        $edit_level   = mysqli_real_escape_string($conn, $_POST['edit-level']);
+        $edit_section = mysqli_real_escape_string($conn, $_POST['edit-section']);
+        $query        = "UPDATE courses SET course = ?, course_code = ?, level = ?, section = ? WHERE id = ?";
+        $params       = [$edit_course, $edit_course, $edit_level, $edit_section, $id];
+        if (executePreparedStatement($conn, $query, $params, "ssssi")) {
+            header("Location: courses.php");
+        }
+    }
+}
+?>
