560ff4db0a47a6ef4a8b203a33208fd73df5d297
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-03-06 13:56:00 +0800

    Install bootstrap dependencies instead.

--- header.php
+++ header.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stock Card System</title>
-    <link rel="stylesheet" href="main_ui/css/bootstrap.min.css">
-    <script type="text/javascript" src="main_ui/js/bootstrap.js"></script>
-    <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <script type=" text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script type=" text/javascript" src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script type=" text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type=" text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
     <link rel="stylesheet" href="node_modules/boxicons/css/boxicons.min.css">
 </head>
